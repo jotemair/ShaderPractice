03faf1fedbdfdd2d2a8379268153b60ec976e6e4
Adam Rudas <jotemair@gmail.com>
2019-09-16 14:52:28 +1000

    Paper and outline shaders

---
+++ Assets/Scripts/DrawingPaper.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Image Effects/Drawing Paper")]
+public class DrawingPaper : MonoBehaviour
+{
+    #region Variables
+
+    public Shader shader = null;
+    private float timeX = 1;
+    public Color pencilColor = new Color(0, 0, 0, 0);
+
+    [Range(0, 2)]
+    public float pencilSize = 0.00125f;
+
+    [Range(0, 2)]
+    public float pencilCorrection = 0.35f;
+
+    [Range(0, 1)]
+    public float intensity = 1f;
+
+    [Range(0, 2)]
+    public float animationSpeed = 1;
+
+    [Range(0, 1)]
+    public float cornerLoss = 1f;
+
+    [Range(0, 1)]
+    public float paperFadeIn = 0f;
+
+    [Range(0, 1)]
+    public float paperFadeColor = 1f;
+
+    public Color backColor = new Color(1, 1, 1, 1);
+    private Material paperMaterial = null;
+    public Texture2D paper = null;
+
+    #endregion
+
+    #region Properties
+
+    Material material
+    {
+        get
+        {
+            if (null == paperMaterial)
+            {
+                paperMaterial = new Material(shader);
+                paper.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            return paperMaterial;
+        }
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (null != paperMaterial)
+        {
+            DestroyImmediate(paperMaterial);
+            paperMaterial = null;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (null != shader)
+        {
+            timeX += Time.deltaTime;
+            if (timeX > 100f)
+            {
+                timeX = 0f;
+            }
+
+            material.SetFloat("_TimeX", timeX);
+
+            material.SetColor("_PencilColor", pencilColor);
+            material.SetColor("_BackColor", backColor);
+
+            material.SetFloat("_PencilSize", pencilSize);
+            material.SetFloat("_PencilCorrection", pencilCorrection);
+            material.SetFloat("_Value3", intensity);
+            material.SetFloat("_Value4", animationSpeed);
+            material.SetFloat("_Value5", cornerLoss);
+            material.SetFloat("_Value6", paperFadeIn);
+            material.SetFloat("_Value7", paperFadeColor);
+
+            material.SetTexture("_MainTex2", paper);
+
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
---
+++ Assets/Shaders/OutlineShader.shader
@@ -0,0 +1,108 @@
+﻿Shader "Custom/OutlineShader"
+{
+    Properties
+    {
+		_Texture("Texture", 2D) = "black" {}
+
+		_OutlineColor("Outline Color", Color) = (1,1,1,1)
+		_OutlineWidth("Outline Width", Range(1.0,10.0)) = 1.1
+    }
+
+    SubShader
+    {
+		// The first pass draws a single color version of the object, but scaled up
+		// The second pass will render the object as is over the first pass
+		Pass
+		{
+			// ZWrite Off also works, but for some reason only in editor view
+			// Call front makes it so we render the back side of the object, so that will make the second pass render over it, because it will be in front of it
+			Cull Front
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			float _OutlineWidth;
+			float4 _OutlineColor;
+
+			v2f vert(appdata IN)
+			{
+				// Scale up the local vertex positions by the outline width value to make the object appear bigger
+				IN.vertex.xyz *= _OutlineWidth;
+				v2f OUT;
+
+				OUT.pos = UnityObjectToClipPos(IN.vertex);
+				OUT.uv = IN.uv;
+
+				return OUT;
+			}
+
+			fixed4 frag(v2f IN) : SV_Target
+			{
+				return _OutlineColor;
+			}
+
+			ENDCG
+		}
+
+		// The second pass is a vertex shader for texture rendering
+		Pass
+		{
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+			
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			sampler2D _Texture;
+			
+			v2f vert(appdata IN)
+			{
+				v2f OUT;
+
+				OUT.pos = UnityObjectToClipPos(IN.vertex);
+				OUT.uv = IN.uv;
+
+				return OUT;
+			}
+
+			fixed4 frag(v2f IN) : SV_Target
+			{
+				float4 texColor = tex2D(_Texture, IN.uv);
+				return texColor;
+			}
+
+			ENDCG
+		}
+    }
+
+    FallBack "Diffuse"
+}
